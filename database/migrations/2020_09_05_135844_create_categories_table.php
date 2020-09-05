8c5cb5b638a8468d4452690fd7271e8e8305f048
Avishek <avishekbarua02@gmail.com>
2020-09-05 20:01:26 +0600

    new start

--- database/migrations/2020_09_05_135844_create_categories_table.php
+++ database/migrations/2020_09_05_135844_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCatagoriesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateCatagoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('catagories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug')->nullabel();
-            $table->boolean('status')->nullabel();
-            $table->integer('sort_order')->nullabel();
+            
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateCatagoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('catagories');
+        Schema::dropIfExists('categories');
     }
 }
